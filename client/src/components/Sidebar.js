43381a549a45ba683a38952fc5a6ef03a3da3755
Brian <b.perez3237@gmail.com>
2022-11-04 17:20:27 -0400

    Add toggle sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../App.css'
 import useFetch from '../hooks/useFetch'
 
 
 function Sidebar() {
+    const [showSidebar, setShowSidebar] = useState(false)
     const {data: projects, setData: setProjects} = useFetch('/projects')
 
+    function toggleSidebar() {
+        setShowSidebar(!showSidebar)
+    }
+
+
     return (
-        <div className='Sidebar'>
+        <div className='Sidebar' onClick={toggleSidebar} style={{width:(showSidebar ? '5%' : '30%')}}>
             <ul className='Sidebar-list'>
                 {projects.map((project, key) => {
                 return (
                     <li key={key} className='row' onClick={()=>{window.open(project.link)}}>
-                        <div id='icon'><h1>X</h1></div>
-                        <div id='title'>{project.title}</div>
+                        <div id='icon' style={{display:(showSidebar ? 'none' : 'block')}}><h1>X</h1></div>
+                        <div id='title' style={{display:(showSidebar ? 'none' : 'block')}}>{project.title}</div>
                     </li>
                 )})}
             </ul>
