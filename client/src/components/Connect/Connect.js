93139568cf380cbd3058e1fe0c47289c37eac7fe
Brian <b.perez3237@gmail.com>
2022-11-05 14:37:05 -0400

    Setup Connect page

--- client/src/components/Connect/Connect.js
+++ client/src/components/Connect/Connect.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Connect.css';
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
+import {FaDev} from 'react-icons/fa'
 
 function Connect() {
   return (
-    <div className='main'>Connect</div>
+    <div className='main'>
+      <h1>Connect</h1>
+      <p>intro paragraph here</p>
+      <div className='links-group'>
+        <AiFillGithub className='link' onClick={()=>window.open('https://github.com/bperez3237')}/>
+        <AiFillLinkedin className='link' onClick={()=>window.open('https://www.linkedin.com/in/brian-perez-164a6b155/')}/>
+        <FaDev className='link' onClick={()=>window.open('https://dev.to/bperez3237')}/>
+        <p>Email: b.perez3237@gmail.com</p>
+      </div>
+    </div>
   )
 }
 
